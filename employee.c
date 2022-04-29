67e7902bbb46adfb1e4abb98a28ab005910254fc
B-RAMKUMAR <103769152+B-RAMKUMAR@users.noreply.github.com>
2022-04-29 07:04:26 +0530

    Employees detail
    Employee database

---
+++ employee.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct{
+    char name[30];
+    int id;
+    double salary;
+} Employee;
+ 
+int main()
+{
+    //number of employees
+    int n=20;
+    
+    //array to store structure values of all employees
+    Employee employees[n];
+ 
+    //Taking each employee detail as input
+    printf("Enter %d Employee Details \n \n",n);
+        printf("Name:","Id","Salary\n");
+        
+    for(int i=0; i<n; i++){
+        printf("Employee %d:- \n",i+1);
+
+        //Name
+        printf("Name: ");
+        scanf("%[^\n]s",employees[i].name);
+
+        //ID
+        printf("Id: ");
+        scanf("%d",&employees[i].id);
+
+        //Salary
+        printf("Salary: ");
+        scanf("%lf",&employees[i].salary);
+
+        //to consume extra '\n' input
+        char ch = getchar();
+ 
+        printf("\n");
+    }
+ 
+    //Displaying Employee details
+    printf("-------------- All Employees Details ---------------\n");
+    for(int i=0; i<n; i++){
+        printf("Name \t: ");
+        printf("%s \t",employees[i].name);
+ 
+        printf("Id \t: ");
+        printf("%d \t",employees[i].id);
+ 
+        printf("Salary \t: ");
+        printf("%.2lf \t",employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    return 0;
+}
